3f3c4f2928d59794b6c973419e95036950325b1f
iskandar rafikov <isko.85@mail.ru>
2023-03-14 18:00:41 +0300

    kotlin.basic Выполнение заданий 1,2,3

--- app/src/main/java/ru/education/homework_1/Main.kt
+++ app/src/main/java/ru/education/homework_1/Main.kt
@@ -1,5 +1,38 @@
 package ru.education.homework_1
 
-fun main(args: Array<String>) {
+import kotlin.system.measureTimeMillis
 
+fun main() {
+    //  Задание 1
+    val number = (0..99).toList()
+        .filter { (it % 2) == 0 }
+        .slice(10..20)
+        .map { it + 1 }
+        .sum()
+
+    println("sum = $number")
+
+    // Задание 2
+    val planetExpress = listOf<Person>()
+        .plus(Person("Philip", "Fry", 25))
+        .plus(Person("Turanga", "Leela", 24))
+        .plus(Person("Bender", "Rodriquez", 5))
+        .plus(Person("John", "Zoidberg", 88))
+
+    val sortedByAge = planetExpress.sortByAge()
+    println(sortedByAge)
+
+    val sortedByName = planetExpress.sortByName()
+    println(sortedByName)
+
+    // Задание 3
+    fun measureSortAlgo(container: List<Int>, sort: (List<Int>) -> Unit) : Long {
+        return measureTimeMillis { sort(container) }
+    }
+
+    val list = (0..10000).toList()
+        .shuffled()
+    val result = measureSortAlgo(list) { l: List<Int> -> l.sorted() }
+
+    println("Время работы алгоритма: $result ms")
 }
--- app/src/main/java/ru/education/homework_1/Person.kt
+++ app/src/main/java/ru/education/homework_1/Person.kt
@@ -4,6 +4,15 @@ data class Person(
     val name: String,
     val surname: String,
     val age: Int
-) {
-    //
+)
+
+fun List<Person>.sortByAge():
+        List<Person> {
+    val list = sortedWith(compareBy { it.age }).reversed()
+    return list
+}
+
+fun List<Person>.sortByName():
+        List<Person> {
+    return sortedWith(compareBy ({ it.name }, { it.surname }))
 }
